d09996fc7214707b18b7996276f760f8c16988f9
BMontijo <juansonos@gmail.com>
2018-12-05 12:13:55 -0500

    Add files, define book, build book_card_view layout

---
+++ app/src/main/java/Book.java
@@ -0,0 +1,11 @@
+// Represents a book object
+// holds Title, Author, Publish Year
+public class Book {
+    private String mBookTitle;
+    private String mAuthor;
+    private String mPubYear;
+
+    public Book (String bTitle, String bAuthor, String bPubYear) {
+
+    }
+}
---
+++ app/src/main/java/Utils.java
@@ -0,0 +1,9 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public final class Utils {
+    // private constructor
+    private Utils() {}
+
+
+}
